5d02a63742b8bfacb99f79f0f0dd85641f87b2fa
Brian Hill <brian.l.hill@icloud.com>
2017-05-25 16:49:01 -0700

    Run script now has default, or takes in paramter; also includes ODA paths

--- run.sh
+++ run.sh
@@ -4,8 +4,13 @@ classpath="$HOME/code/github/gatk/build/classes/main"
 classpath="$HOME/code/github/htsjdk/build/classes/main:$classpath"
 classpath="$HOME/code/github/gatk/build/install/gatk/lib:$classpath"
 classpath="$HOME/code/github/gatk/build/libs/gatk-package-4.alpha.2-171-g92cb860-SNAPSHOT-local.jar:$classpath"
+classpath="$HOME/code/github/gatk/build/libs/gatk.jar:$classpath"
 classpath=".:"$classpath
 
 echo "$classpath"
 
-java -Djava.library.path=. -classpath $classpath HelloJNI wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam
+if [ ! -z "$1" ]; then
+    java -Djava.library.path=. -classpath $classpath HelloJNI $1
+else    
+    java -Djava.library.path=. -classpath $classpath HelloJNI wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam 
+fi
